9beb48ed4dcc858d1cdefc0567fc3ce2f9faf200
Tom Greenfield <tom.greenfield@kineo.com>
2021-08-24 15:56:35 +0100

    Destructure; return early

--- utils/stripObject.js
+++ utils/stripObject.js
@@ -1,7 +1,5 @@
 export default function stripObject(object) {
 	Object.keys(object).forEach(i => object[i] === undefined && delete object[i]);
 
-	if (!Object.keys(object).length) return;
-
-	return object;
+	if (Object.keys(object).length) return object;
 };
